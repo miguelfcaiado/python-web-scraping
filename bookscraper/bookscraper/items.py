01bdf709775a049c3d746986f1cb7f008ec4f2a3
miguelfcaiado <caiado.miguelwork@gmail.com>
2024-03-30 14:54:15 +0000

    Saving data to files and databases

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -17,6 +17,7 @@ class BookscraperItem(scrapy.Item):
 class BookItem(scrapy.Item):
     url = scrapy.Field()
     title = scrapy.Field()
+    upc = scrapy.Field()
     product_type = scrapy.Field()
     price_excl_tax = scrapy.Field()
     price_incl_tax = scrapy.Field()
--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,6 +7,12 @@ class BookspiderSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
+    custom_settings = {
+        'FEEDS': {
+            'bookdata.json': {'format': 'json', 'overwrite': True},
+        }
+    }
+
     def parse(self, response):
         books = response.css('article.product_pod')
 
@@ -37,6 +43,7 @@ class BookspiderSpider(scrapy.Spider):
 
         book_item['url'] = response.url,
         book_item['title'] = response.css('.product_main h1::text').get(),
+        book_item['upc'] = table_rows[0].css("td ::text").get()
         book_item['product_type'] = table_rows[1].css("td ::text").get(),
         book_item['price_excl_tax'] = table_rows[2].css("td ::text").get(),
         book_item['price_incl_tax'] = table_rows[3].css("td ::text").get(),
